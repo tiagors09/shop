3540adc5fb5252629b180ada7ba9c69cab05ac2a
Tiago Rodrigues <tiagorodriguessousa9@gmail.com>
2024-04-01 13:25:22 -0300

    products_overview_screen.dart: added grid view

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:shop/data/dummy_data.dart';
+
+import '../models/product.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
-  const ProductsOverviewScreen({super.key});
+  final List<Product> loadedProducts = dummyProducts;
+
+  ProductsOverviewScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Minha Loja'),
+      ),
+      body: GridView.builder(
+        itemCount: loadedProducts.length,
+        padding: const EdgeInsets.all(10),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        itemBuilder: (ctx, i) {
+          return Text(
+            loadedProducts[i].title,
+          );
+        },
+      ),
+    );
   }
 }
