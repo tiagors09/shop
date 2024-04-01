e67918a4b07c477853390249b149c22db710037a
Tiago Rodrigues <tiagorodriguessousa9@gmail.com>
2024-04-01 12:57:53 -0300

    added product class

---
+++ lib/models/product.dart
@@ -0,0 +1,15 @@
+class ClassName {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+
+  ClassName({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+  });
+}
