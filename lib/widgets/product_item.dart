91bb6a0cdbc34d0595539966bafd0ca43e011d8a
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-04-05 11:14:44 -0300

    product_item.dart: added gesture detector and errorBuilder when image fail to load

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shop/models/product.dart';
+import 'package:shop/utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
   final Product product;
@@ -13,31 +14,43 @@ class ProductItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
-      child: GridTile(
-        footer: GridTileBar(
-          trailing: IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.shopping_cart,
-              color: Theme.of(context).colorScheme.secondary,
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context).pushNamed(
+            AppRoutes.productDetail.name,
+            arguments: product,
+          );
+        },
+        child: GridTile(
+          footer: GridTileBar(
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Theme.of(context).colorScheme.secondary,
+              ),
+            ),
+            title: Text(
+              product.title,
+              textAlign: TextAlign.center,
+            ),
+            backgroundColor: Colors.black87,
+            leading: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.favorite,
+                color: Theme.of(context).colorScheme.secondary,
+              ),
             ),
           ),
-          title: Text(
-            product.title,
-            textAlign: TextAlign.center,
-          ),
-          backgroundColor: Colors.black87,
-          leading: IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.favorite,
-              color: Theme.of(context).colorScheme.secondary,
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
+            errorBuilder: (context, error, stackTrace) => Image.network(
+              'https://via.placeholder.com/600/92c952',
+              fit: BoxFit.cover,
             ),
           ),
-        ),
-        child: Image.network(
-          product.imageUrl,
-          fit: BoxFit.cover,
         ),
       ),
     );
