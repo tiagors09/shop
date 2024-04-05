d81f64f3b6ed5acba4e8ee15ce841ece9bd15aff
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-04-05 11:12:41 -0300

    product_detail_screen.dart: using route argument instead of param

--- lib/views/product_detail_screen.dart
+++ lib/views/product_detail_screen.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:shop/models/product.dart';
 
-class ProductDetailScreen extends StatelessWidget {
-  final Product product;
-
+class ProductDetailScreen extends StatefulWidget {
   const ProductDetailScreen({
     super.key,
-    required this.product,
   });
 
   @override
+  State<ProductDetailScreen> createState() => _ProductDetailScreenState();
+}
+
+class _ProductDetailScreenState extends State<ProductDetailScreen> {
+  @override
   Widget build(BuildContext context) {
+    final Product product =
+        ModalRoute.of(context)?.settings.arguments as Product;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(product.description),
+        title: Text(product.title),
       ),
     );
   }
