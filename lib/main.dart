9bd719786d3abe075465fcc9193206e6fcc2dd73
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-04-08 11:09:30 -0300

    main.dart: wrap MaterialApp with ChangeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/providers/products.dart';
 import 'package:shop/utils/app_routes.dart';
 import 'package:shop/views/product_detail_screen.dart';
 import 'package:shop/views/products_overview_screen.dart';
@@ -11,19 +13,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = ThemeData();
-    return MaterialApp(
-      routes: {
-        AppRoutes.productDetail.name: (ctx) => const ProductDetailScreen(),
-      },
-      title: 'Minha Loja',
-      theme: ThemeData(
-        colorScheme: theme.colorScheme.copyWith(
-          primary: Colors.purple,
-          secondary: Colors.deepOrange,
+    return ChangeNotifierProvider(
+      create: (_) => Products(),
+      child: MaterialApp(
+        routes: {
+          AppRoutes.productDetail.name: (ctx) => const ProductDetailScreen(),
+        },
+        title: 'Minha Loja',
+        theme: ThemeData(
+          colorScheme: theme.colorScheme.copyWith(
+            primary: Colors.purple,
+            secondary: Colors.deepOrange,
+          ),
+          fontFamily: 'Lato',
         ),
-        fontFamily: 'Lato',
+        home: const ProductsOverviewScreen(),
       ),
-      home: ProductsOverviewScreen(),
     );
   }
 }
