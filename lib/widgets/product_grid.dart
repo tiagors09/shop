e06ee2ff6cdb29cd934b444c730c77ea1854097d
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-04-08 11:10:15 -0300

    added product grid

---
+++ lib/widgets/product_grid.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/product.dart';
+import '../providers/products.dart';
+import 'product_item.dart';
+
+class ProductGrid extends StatelessWidget {
+  const ProductGrid({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final productsProvider = Provider.of<Products>(context);
+    final List<Product> loadedProducts = productsProvider.items;
+
+    return GridView.builder(
+      itemCount: loadedProducts.length,
+      padding: const EdgeInsets.all(10),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemBuilder: (ctx, i) => ProductItem(
+        product: loadedProducts[i],
+      ),
+    );
+  }
+}
