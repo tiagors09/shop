212958824d023128b42a5784d27aefc5657c971f
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-04-08 11:10:07 -0300

    products_overview_screen.dart: extracted product grid to new widget

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:shop/data/dummy_data.dart';
-import 'package:shop/widgets/product_item.dart';
 
-import '../models/product.dart';
+import '../widgets/product_grid.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
-  final List<Product> loadedProducts = dummyProducts;
-
-  ProductsOverviewScreen({super.key});
+  const ProductsOverviewScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,19 +11,7 @@ class ProductsOverviewScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Minha Loja'),
       ),
-      body: GridView.builder(
-        itemCount: loadedProducts.length,
-        padding: const EdgeInsets.all(10),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        itemBuilder: (ctx, i) => ProductItem(
-          product: loadedProducts[i],
-        ),
-      ),
+      body: const ProductGrid(),
     );
   }
 }
