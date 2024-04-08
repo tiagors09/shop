9221b4b372eba85615dc374914eedc6746dcb38d
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-04-08 11:09:41 -0300

    add Products

---
+++ lib/providers/products.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:shop/data/dummy_data.dart';
+
+import '../models/product.dart';
+
+class Products with ChangeNotifier {
+  final List<Product> _items = dummyProducts;
+
+  List<Product> get items => [..._items];
+
+  void addProduct(Product product) {
+    _items.add(product);
+    notifyListeners();
+  }
+}
