a79be8faa0d997fe9f0d826c762c27210c0c1c18
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-04-15 12:13:25 -0300

    product_item.dart: added cart provider

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/utils/app_routes.dart';
 
+import '../providers/cart.dart';
 import '../providers/product.dart';
 
 class ProductItem extends StatelessWidget {
@@ -11,7 +12,8 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final Product product = Provider.of<Product>(context);
+    final Product product = Provider.of<Product>(context, listen: false);
+    final Cart cart = Provider.of<Cart>(context, listen: false);
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
@@ -25,10 +27,7 @@ class ProductItem extends StatelessWidget {
         child: GridTile(
           footer: GridTileBar(
             trailing: IconButton(
-              onPressed: () {
-                // TODO: lógica para adicionar
-                // produto ao carrinho
-              },
+              onPressed: () => cart.addItem(product),
               icon: Icon(
                 Icons.shopping_cart,
                 color: Theme.of(context).colorScheme.secondary,
