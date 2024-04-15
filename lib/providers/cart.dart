4f51f8ac175e0d217442fef6a4cb5c58499fcc1b
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-04-15 12:11:49 -0300

    cart.dart: added getter to obtain total amount and using ternary struct in addItem

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -18,34 +18,38 @@ class CartItem {
 }
 
 class Cart with ChangeNotifier {
-  late Map<String, CartItem> _items;
+  Map<String, CartItem> _items = {};
 
   Map<String, CartItem> get items => {..._items};
 
   int get itemsCount => _items.length;
 
+  double get totalAmount => _items.values.fold<double>(
+        0.0,
+        (total, currentCartItem) =>
+            total + (currentCartItem.price * currentCartItem.quantity),
+      );
+
   void addItem(Product product) {
-    if (_items.containsKey(product.id)) {
-      _items.update(
-        product.id,
-        (existingItem) => CartItem(
-          id: existingItem.id,
-          title: existingItem.title,
-          quantity: existingItem.quantity + 1,
-          price: existingItem.price,
-        ),
-      );
-    } else {
-      _items.putIfAbsent(
-        product.id,
-        () => CartItem(
-          id: Random().nextDouble().toString(),
-          title: product.title,
-          quantity: 1,
-          price: product.price,
-        ),
-      );
-    }
+    _items.containsKey(product.id)
+        ? _items.update(
+            product.id,
+            (existingItem) => CartItem(
+              id: existingItem.id,
+              title: existingItem.title,
+              quantity: existingItem.quantity + 1,
+              price: existingItem.price,
+            ),
+          )
+        : _items.putIfAbsent(
+            product.id,
+            () => CartItem(
+              id: Random().nextDouble().toString(),
+              title: product.title,
+              quantity: 1,
+              price: product.price,
+            ),
+          );
 
     notifyListeners();
   }
