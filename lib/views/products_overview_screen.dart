63841c725e8e2bbfab2319795dec1fefbfd1011e
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-04-15 09:30:07 -0300

    product_overview_screen.dart: added logic to show favorite products

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../widgets/product_grid.dart';
+import '../providers/products.dart';
+
+enum FilterOptions {
+  favorite,
+  all,
+}
 
 class ProductsOverviewScreen extends StatelessWidget {
   const ProductsOverviewScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final Products products = Provider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha Loja'),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) =>
+                selectedValue == FilterOptions.favorite
+                    ? products.showFavoriteOnly()
+                    : products.showAll(),
+            icon: const Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                value: FilterOptions.favorite,
+                child: Text('Somente Favoritos'),
+              ),
+              const PopupMenuItem(
+                value: FilterOptions.all,
+                child: Text('Todos'),
+              ),
+            ],
+          ),
+        ],
       ),
       body: const ProductGrid(),
     );
