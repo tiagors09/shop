de217dc9988e3ef5401a137b589625e6192ef9e7
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-04-16 09:11:16 -0300

    product_detail_screen.dart: showing product

--- lib/views/product_detail_screen.dart
+++ lib/views/product_detail_screen.dart
@@ -20,6 +20,41 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
       appBar: AppBar(
         title: Text(product.title),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 0,
+              ),
+              child: Text(
+                'R\$ ${product.price}',
+                style: const TextStyle(
+                  color: Colors.grey,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                product.description,
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
