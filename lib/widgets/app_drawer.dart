bc1249a9c3609ac1a41717f1947ece4ad2de93a1
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-04-16 10:53:02 -0300

    added app drawer

---
+++ lib/widgets/app_drawer.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:shop/utils/app_routes.dart';
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({super.key});
+
+  List<Widget> drawnerItemm(
+    BuildContext context,
+    String title,
+    IconData icon,
+    AppRoutes route,
+  ) {
+    return [
+      const Divider(),
+      ListTile(
+        title: Text(title),
+        leading: Icon(icon),
+        onTap: () => Navigator.of(context).pushReplacementNamed(route.name),
+      )
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          AppBar(
+            title: const Text('Bem vindo Usuário'),
+            automaticallyImplyLeading: false,
+          ),
+          ...drawnerItemm(
+            context,
+            "Loja",
+            Icons.shop,
+            AppRoutes.productsOverviewScreen,
+          ),
+          ...drawnerItemm(
+            context,
+            "Pedidos",
+            Icons.payment,
+            AppRoutes.orders,
+          ),
+        ],
+      ),
+    );
+  }
+}
