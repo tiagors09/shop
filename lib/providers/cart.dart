59bd4fc560f9cca2f4ddd2338f46374c1ffe77cb
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-04-17 11:56:40 -0300

    cart.dart: added method to remove a single item

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -58,6 +58,23 @@ class Cart with ChangeNotifier {
     notifyListeners();
   }
 
+  void removeSingleItem(String productId) {
+    if (!_items.containsKey(productId)) return;
+
+    _items[productId]!.quantity == 1
+        ? _items.remove(productId)
+        : _items.update(
+            productId,
+            (existingItem) => CartItem(
+              id: existingItem.id,
+              productId: existingItem.productId,
+              title: existingItem.title,
+              quantity: existingItem.quantity - 1,
+              price: existingItem.price,
+            ),
+          );
+  }
+
   void removeItem(String productId) {
     _items.remove(productId);
     notifyListeners();
