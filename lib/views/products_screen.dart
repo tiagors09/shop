6c858e1f9c1ca51a7c62ad3addf67c8903e9ffde
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-04-18 15:21:00 -0300

    products_scree.dart: added action to add product

--- lib/views/products_screen.dart
+++ lib/views/products_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/providers/products.dart';
 import 'package:shop/widgets/app_drawer.dart';
 
 class ProductsScreen extends StatelessWidget {
@@ -9,8 +11,23 @@ class ProductsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Gerenciar Produtos'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          ),
+        ],
       ),
       drawer: const AppDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Consumer<Products>(
+          builder: (ctx, products, _) => ListView.builder(
+            itemCount: products.itemsCount,
+            itemBuilder: (ctx, i) => const Placeholder(),
+          ),
+        ),
+      ),
     );
   }
 }
