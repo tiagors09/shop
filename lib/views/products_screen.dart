e46be69207672e5ece6c2264fff915835af889cd
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-04-18 15:02:08 -0300

    products_screen.dart: added scaffold, app bar and drawner

--- lib/views/products_screen.dart
+++ lib/views/products_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:shop/widgets/app_drawer.dart';
 
 class ProductsScreen extends StatelessWidget {
   const ProductsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Gerenciar Produtos'),
+      ),
+      drawer: const AppDrawer(),
+    );
   }
 }
