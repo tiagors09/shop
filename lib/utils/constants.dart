0fe9cc1ce6bd57928a51e68177e3be0080cef3be
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-04-22 13:56:23 -0300

    renamed to environment

--- lib/utils/constants.dart
+++
@@ -1,4 +0,0 @@
-abstract class Constants {
-  static const baseUrl = String.fromEnvironment('BASE_URL');
-  static const productsPath = String.fromEnvironment('PRODUCTS_PATH');
-}
---
+++ lib/utils/environment.dart
@@ -0,0 +1,4 @@
+abstract class Environment {
+  static const baseUrl = String.fromEnvironment('BASE_URL');
+  static const productsPath = String.fromEnvironment('PRODUCTS');
+}
