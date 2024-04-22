719cff0bc2aecd296ca541a6d168bc88924b448e
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-04-22 13:23:47 -0300

    products.dart: persisting product in firebase

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,14 +1,19 @@
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:shop/data/dummy_data.dart';
 
+import '../utils/constants.dart';
 import 'product.dart';
 
 class Products with ChangeNotifier {
   final List<Product> _items = dummyProducts;
 
   bool _showFavoriteOnly = false;
+
+  final _url = Uri.parse(Constants.baseUrl + Constants.productsPath);
 
   List<Product> get items => _showFavoriteOnly
       ? _items.where((prod) => prod.isFavorite).toList()
@@ -26,15 +31,34 @@ class Products with ChangeNotifier {
     notifyListeners();
   }
 
-  void addProduct(Product product) {
-    _items.add(Product(
-      id: Random().nextDouble().toString(),
-      title: product.title,
-      description: product.description,
-      price: product.price,
-      imageUrl: product.imageUrl,
-    ));
-    notifyListeners();
+  Future<void> addProduct(Product product) {
+    return http
+        .post(
+      _url,
+      body: json.encode(
+        {
+          'title': product.title,
+          'description': product.description,
+          'price': product.price,
+          'imageUrl': product.imageUrl,
+          'isFavorite': product.isFavorite,
+        },
+      ),
+    )
+        .then(
+      (value) {
+        _items.add(
+          Product(
+            id: Random().nextDouble().toString(),
+            title: product.title,
+            description: product.description,
+            price: product.price,
+            imageUrl: product.imageUrl,
+          ),
+        );
+        notifyListeners();
+      },
+    );
   }
 
   void updateProduct(Product product) {
