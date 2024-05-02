8171010bbd063351215f411b86c11ee2b5b68b65
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-05-02 15:06:54 -0300

    product_grid.dart: showing when not have products in db

--- lib/widgets/product_grid.dart
+++ lib/widgets/product_grid.dart
@@ -15,19 +15,21 @@ class ProductGrid extends StatelessWidget {
     final productsProvider = Provider.of<Products>(context);
     final List<Product> loadedProducts = productsProvider.items;
 
-    return GridView.builder(
-      itemCount: loadedProducts.length,
-      padding: const EdgeInsets.all(10),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-      ),
-      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
-        value: loadedProducts[i],
-        child: const ProductItem(),
-      ),
-    );
+    return loadedProducts.isNotEmpty
+        ? GridView.builder(
+            itemCount: loadedProducts.length,
+            padding: const EdgeInsets.all(10),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+            ),
+            itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+              value: loadedProducts[i],
+              child: const ProductItem(),
+            ),
+          )
+        : const Center(child: Text('Não há produtos para serem exibidos'));
   }
 }
