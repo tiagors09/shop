9c081afabd5de1c304b51a3b490365fd8b0dcb4a
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-05-02 15:05:24 -0300

    environment.dart: added exceptions msgs

--- lib/utils/environment.dart
+++ lib/utils/environment.dart
@@ -1,4 +1,21 @@
 abstract class Environment {
   static const baseUrl = String.fromEnvironment('BASE_URL');
   static const productsPath = String.fromEnvironment('PRODUCTS_PATH');
+
+  // Mensagens fofinhas de erro
+  static const String updateError =
+      "Oops! Parece que encontramos um probleminha ao atualizar o produto. 🙈";
+  static const String insertError =
+      "Ai, ai! Parece que houve um probleminha ao inserir o produto. 🐾";
+  static const String deleteError =
+      "Oh, não! Encontramos um probleminha ao excluir o produto. 🐶";
+  static const String readError =
+      "Eita! Algo deu errado ao ler o produto. Será que ele se escondeu? 🙊";
+
+  // Mensagem de erro para todos os produtos
+  static const String allProductsError =
+      "Ops! Algo deu errado ao processar todos os produtos. 🐱";
+
+  // Título da caixa de diálogo
+  static const String dialogTitle = "Opsie! Probleminha";
 }
