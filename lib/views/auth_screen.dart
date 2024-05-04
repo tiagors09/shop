28a91b3c5f7d8541dfd428d4da55680c3920a6d9
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-05-04 11:09:02 -0300

    added auth screen

---
+++ lib/views/auth_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatelessWidget {
+  const AuthScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromRGBO(215, 117, 255, 0.5),
+                  Color.fromRGBO(255, 188, 177, 0.9),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            child: Column(
+              children: [],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
