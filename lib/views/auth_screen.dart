beaf5a2689c5317a84adcd5f8e6a0a73d0296b79
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-05-04 11:23:39 -0300

    auth_screen.dart: added banner

--- lib/views/auth_screen.dart
+++ lib/views/auth_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class AuthScreen extends StatelessWidget {
@@ -20,10 +22,39 @@ class AuthScreen extends StatelessWidget {
               ),
             ),
           ),
-          Container(
+          SizedBox(
             width: double.infinity,
             child: Column(
-              children: [],
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  transform: Matrix4.rotationZ(-2 * pi / 45)..translate(-10.0),
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 70,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.deepOrange.shade900,
+                    boxShadow: const [
+                      BoxShadow(
+                        blurRadius: 8,
+                        color: Colors.black26,
+                        offset: Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: const Text(
+                    'Minha Loja',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Anton',
+                      fontSize: 45,
+                    ),
+                  ),
+                )
+              ],
             ),
           )
         ],
