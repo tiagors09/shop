1e238d685a18fa5ca3a00a419e25fb3a257cb53b
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-05-04 10:34:39 -0300

    orders_screen.dart: added future builder

--- lib/views/orders_screen.dart
+++ lib/views/orders_screen.dart
@@ -1,52 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop/utils/environment.dart';
 import 'package:shop/widgets/app_drawer.dart';
 import 'package:shop/widgets/order_widget.dart';
 
 import '../providers/order.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   const OrdersScreen({super.key});
-
-  @override
-  State<OrdersScreen> createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _onRefresh();
-  }
-
-  Future<void> _onRefresh() {
-    return Provider.of<Orders>(
-      context,
-      listen: false,
-    )
-        .loadOrders()
-        .then(
-          (_) => setState(
-            () {
-              _isLoading = false;
-            },
-          ),
-        )
-        .catchError(
-      (e) {
-        Environment.showErrorMessage(
-          context,
-          e.toString(),
-        );
-        setState(() {
-          _isLoading = false;
-        });
-      },
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,25 +15,32 @@ class _OrdersScreenState extends State<OrdersScreen> {
       appBar: AppBar(
         title: const Text('Meus Pedidos'),
       ),
-      body: _isLoading
-          ? const Center(
+      body: FutureBuilder(
+        future: Provider.of<Orders>(
+          context,
+          listen: false,
+        ).loadOrders(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
               child: CircularProgressIndicator(),
-            )
-          : Consumer<Orders>(
-              builder: (ctx, orders, child) => orders.itemsCount > 0
-                  ? RefreshIndicator(
-                      onRefresh: _onRefresh,
-                      child: ListView.builder(
-                        itemCount: orders.itemsCount,
-                        itemBuilder: (ctx, i) => OrderWidget(
-                          order: orders.items[i],
-                        ),
-                      ),
-                    )
-                  : const Center(
-                      child: Text('Não há pedidos'),
-                    ),
-            ),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text(snapshot.error.toString()),
+            );
+          } else {
+            return Consumer<Orders>(
+              builder: (ctx, orders, child) => ListView.builder(
+                itemCount: orders.itemsCount,
+                itemBuilder: (ctx, i) => OrderWidget(
+                  order: orders.items[i],
+                ),
+              ),
+            );
+          }
+        },
+      ),
     );
   }
 }
