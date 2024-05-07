b9e811570040499368a40564c9d21074960babf8
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-05-07 09:10:32 -0300

    added auth or home screen

---
+++ lib/views/auth_or_home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/providers/auth.dart';
+import 'package:shop/views/auth_screen.dart';
+import 'package:shop/views/products_overview_screen.dart';
+
+class AuthOrHomeScreen extends StatelessWidget {
+  const AuthOrHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final auth = Provider.of<Auth>(context);
+
+    return auth.isAuth ? const ProductsOverviewScreen() : const AuthScreen();
+  }
+}
