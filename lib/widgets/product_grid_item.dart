d4bf1e75ad1060228d3997026078d0687a6ae896
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-05-13 12:48:13 -0300

    product_grid_item.dart: added hero

--- lib/widgets/product_grid_item.dart
+++ lib/widgets/product_grid_item.dart
@@ -65,11 +65,15 @@ class ProductItem extends StatelessWidget {
               ),
             ),
           ),
-          child: Image.network(
-            product.imageUrl,
-            fit: BoxFit.cover,
-            errorBuilder: (context, error, stackTrace) => Image.network(
-              'https://via.placeholder.com/600/92c952',
+          child: Hero(
+            tag: product.id!,
+            child: FadeInImage(
+              placeholder: const AssetImage(
+                'assets/images/product-placeholder.png',
+              ),
+              image: NetworkImage(
+                product.imageUrl,
+              ),
               fit: BoxFit.cover,
             ),
           ),
